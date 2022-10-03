52e2ebaf609f7707fab62f57a80d4b286becb17c
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-10-03 11:27:59 -0300

    ✨feat: exporting external fonts and creating global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { Post } from "./Post";
+import "./global.css";
 
 export function App() {
   return (
     <div>
       <h1>Hello world</h1>
-      <Post
-        author="Felipe Bastos"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ducimus ipsa hic aut voluptatum veritatis accusamus, natus ad, rerum dignissimos alias maiores blanditiis repellat, error debitis sunt placeat beatae iste?"
-      />
-      <Post
-        author="Felipe Bastos"
-        content="Um post muito legal"
-      />
     </div>
   );
 }
