f7371ab009d03329272e165e5837a89c83ec10cb
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-10-03 11:34:38 -0300

    ✨ feat: creating header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header";
 import "./global.css";
 
 export function App() {
   return (
     <div>
+      <Header />
       <h1>Hello world</h1>
     </div>
   );
