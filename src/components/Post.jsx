ac7539f86721a0ff29d7313d7935d7d9eeaa764d
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-10-03 15:30:55 -0300

    ✨ feat: creating avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/felipecfb.png"
-          />
+          <Avatar src="https://github.com/felipecfb.png" />
           <div className={styles.authorInfo}>
             <strong>Felipe Bastos</strong>
             <span>Web Developer</span>
