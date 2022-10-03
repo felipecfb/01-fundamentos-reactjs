3e80525bb7675555f57e2274b57d000749960187
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-10-03 11:10:30 -0300

    ✨feat: working with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-function App() {
+import { Post } from "./Post"
+
+export function App() {
   return (
-    <h1>Hello world</h1>
+    <div>
+      <h1>Hello world</h1>
+      <Post />
+    </div>
   )
 }
-
-export default App
