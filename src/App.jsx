fb80b83b52fe388709d1923df4487a00f11a086a
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-10-03 11:14:06 -0300

    ✨ feat: working with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Post } from "./Post"
+import { Post } from "./Post";
 
 export function App() {
   return (
     <div>
       <h1>Hello world</h1>
-      <Post />
+      <Post
+        author="Felipe Bastos"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ducimus ipsa hic aut voluptatum veritatis accusamus, natus ad, rerum dignissimos alias maiores blanditiis repellat, error debitis sunt placeat beatae iste?"
+      />
+      <Post
+        author="Felipe Bastos"
+        content="Um post muito legal"
+      />
     </div>
-  )
+  );
 }
