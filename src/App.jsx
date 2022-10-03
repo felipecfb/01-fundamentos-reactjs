82b52809a04098f8821f00c9b26eb2308625b4ae
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-10-03 12:11:42 -0300

    ✨ feat: creating sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { Post } from "./Post";
 import { Header } from "./components/Header";
+import { SideBar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <h1>Hello world</h1>
+
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post
+            author="Felipe Bastos"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sequi voluptates veritatis, ratione laboriosam eligendi accusantium culpa corrupti quasi ullam cum unde suscipit quis, iure, autem odio modi harum repellendus!"
+          />
+        </main>
+      </div>
     </div>
   );
 }
